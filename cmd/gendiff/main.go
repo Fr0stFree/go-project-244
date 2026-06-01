7aad6497d068d86643edb8b16e279877ab04c4d6
krasov <magneticmechanicals@gmail.com>
2026-06-01 16:30:47 +0300

    implement cli flag: --format

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -12,8 +12,29 @@ func main() {
 	cmd := &cli.Command{
 		Name:  "gendiff",
 		Usage: "Compares two configuration files and shows a difference.",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "format",
+				Aliases:  []string{"f"},
+				Value:    "stylish",
+				Required: false,
+				Usage:    "output format",
+			},
+		},
 		Action: func(_ context.Context, cmd *cli.Command) error {
-			fmt.Println("Hello, world!")
+			args := cmd.Args()
+			if args.Len() != 2 {
+				return fmt.Errorf("expected 2 arguments: file1 and file2")
+			}
+			firstFP := args.Get(0)
+			secondFP := args.Get(1)
+			format := cmd.String("format")
+			fmt.Printf(
+				"file1=%s file2=%s format=%s\n",
+				firstFP,
+				secondFP,
+				format,
+			)
 			return nil
 		},
 	}
