e88fc909ac5a7ba621a416f435b22e663d3a0b18
krasov <magneticmechanicals@gmail.com>
2026-06-06 14:55:49 +0300

    fix test

--- internal/diff/diff_test.go
+++ internal/diff/diff_test.go
@@ -55,7 +55,7 @@ func TestBuild(t *testing.T) {
 			name:       "should handle nested maps",
 			inputLeft:  map[string]any{"foo": map[string]any{"bar": "baz"}},
 			inputRight: map[string]any{"foo": map[string]any{"bar": "qux"}},
-			expected:   []Record{{"foo", Changed, map[string]any{"bar": "baz"}, map[string]any{"bar": "qux"}, []Record{{"bar", Changed, "baz", "qux", nil}}}},
+			expected:   []Record{{"foo", Nested, nil, nil, []Record{{"bar", Changed, "baz", "qux", nil}}}},
 		},
 	}
 
