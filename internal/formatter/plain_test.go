b75218f8f18514d5809ad91c05e1afbf67622163
krasov <magneticmechanicals@gmail.com>
2026-06-06 16:17:24 +0300

    add tests for plain formatter

---
+++ internal/formatter/plain_test.go
@@ -0,0 +1,68 @@
+package formatter
+
+import (
+	"code/internal/diff"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRender(t *testing.T) {
+	type testCase struct {
+		name     string
+		records  []diff.Record
+		expected string
+	}
+
+	testCases := []testCase{
+		{
+			name: "should render added record",
+			records: []diff.Record{
+				{Key: "foo", State: diff.Added, OldValue: nil, NewValue: "bar", Children: nil},
+			},
+			expected: "Property 'foo' was added with value: 'bar'",
+		},
+		{
+			name: "should render removed record",
+			records: []diff.Record{
+				{Key: "foo", State: diff.Removed, OldValue: "bar", NewValue: nil, Children: nil},
+			},
+			expected: "Property 'foo' was removed",
+		},
+		{
+			name: "should render changed record",
+			records: []diff.Record{
+				{Key: "foo", State: diff.Changed, OldValue: "bar", NewValue: "baz", Children: nil},
+			},
+			expected: "Property 'foo' was updated. From 'bar' to 'baz'",
+		},
+		{
+			name: "should render unchanged record",
+			records: []diff.Record{
+				{Key: "foo", State: diff.Unchanged, OldValue: "bar", NewValue: "bar", Children: nil},
+			},
+			expected: "",
+		},
+		{
+			name: "should render nested records",
+			records: []diff.Record{
+				{
+					Key: "foo", State: diff.Nested, OldValue: nil, NewValue: nil,
+					Children: []diff.Record{
+						{Key: "bar", State: diff.Added, OldValue: nil, NewValue: "baz", Children: nil},
+					},
+				},
+			},
+			expected: "Property 'foo.bar' was added with value: 'baz'",
+		},
+
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			formatter := &plainDiffFormatter{}
+			result := formatter.Render(testCase.records)
+			assert.Equal(t, testCase.expected, result)
+		})
+	}
+}
