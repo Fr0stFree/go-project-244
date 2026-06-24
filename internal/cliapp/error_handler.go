168deaf727ff2550bb2cb7236dadc81408232b5d
krasov <magneticmechanicals@gmail.com>
2026-06-24 21:04:11 +0300

    refactor cli error handling

--- internal/cliapp/error_handler.go
+++ internal/cliapp/error_handler.go
@@ -14,21 +14,29 @@ const defaultErrExitCode = 1
 func toCLIExitError(err error) error {
 	var parseErr *parser.ParseError
 	if errors.As(err, &parseErr) {
-		switch {
-		case errors.Is(err, parser.ErrNoFileExtension):
-			return cli.Exit(fmt.Sprintf("unable to parse file %q: file has no extension", parseErr.Path), defaultErrExitCode)
-		case errors.Is(err, parser.ErrUnsupportedFileType):
-			return cli.Exit(fmt.Sprintf("unable to parse file %q: unsupported file extension", parseErr.Path), defaultErrExitCode)
-		default:
-			return cli.Exit(parseErr.Error(), defaultErrExitCode)
-		}
+		return parseErrorToCLIExitError(err, parseErr)
 	}
 
 	var pathErr *fs.PathError
-	if !errors.As(err, &pathErr) {
-		return err
+	if errors.As(err, &pathErr) {
+		return pathErrorToCLIExitError(err, pathErr)
 	}
 
+	return err
+}
+
+func parseErrorToCLIExitError(err error, parseErr *parser.ParseError) error {
+	switch {
+	case errors.Is(err, parser.ErrNoFileExtension):
+		return cli.Exit(fmt.Sprintf("unable to parse file %q: file has no extension", parseErr.Path), defaultErrExitCode)
+	case errors.Is(err, parser.ErrUnsupportedFileType):
+		return cli.Exit(fmt.Sprintf("unable to parse file %q: unsupported file extension", parseErr.Path), defaultErrExitCode)
+	default:
+		return cli.Exit(parseErr.Error(), defaultErrExitCode)
+	}
+}
+
+func pathErrorToCLIExitError(err error, pathErr *fs.PathError) error {
 	switch {
 	case errors.Is(err, fs.ErrNotExist):
 		return cli.Exit(fmt.Sprintf("unable to read file %q: file does not exist", pathErr.Path), defaultErrExitCode)
